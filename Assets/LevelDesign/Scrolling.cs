13decab404d16a33f32aab7b7caa01d4ff896d6a
Synn <n.64230@hotmail.fr>
2022-04-12 23:13:08 +0200

    Ajout commentaire et ajout spawn rate dégressif

--- Assets/LevelDesign/Scrolling.cs
+++ Assets/LevelDesign/Scrolling.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Scrolling : MonoBehaviour
 {
-
+    // ce speed est connecté et mis a jour par le script barrierBehavior
     public float speed = 1;
 
     // Update is called once per frame
